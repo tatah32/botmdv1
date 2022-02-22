ae26a9b9656bfea3e4fe6fe3a5351e1ec8548859
Nata <94069522+tatah32@users.noreply.github.com>
2022-02-22 10:18:39 +0700

    Update sewa.js

--- ryudata/sewa.js
+++ ryudata/sewa.js
@@ -3,9 +3,9 @@ exports.sewa = () => {
 
 ┏━━━━━━
 ┃𝗛𝗔𝗥𝗚𝗔 𝗦𝗘𝗪𝗔 :
-┃ *1 MINGGU : Rp 5.000*
-┃ *1 BULAN : Rp 15.000*
-┃ *PERMANEN : Rp 35.000*
+┃ *1 MINGGU : Free*
+┃ *1 BULAN : Free*
+┃ *PERMANEN : Free*
 ┗━━━━━━
 ┏━━━━━━
 ┃𝗙𝗜𝗧𝗨𝗥 𝗕𝗢𝗧 :
@@ -14,10 +14,5 @@ exports.sewa = () => {
 ┃ *📥 Downloader*
 ┃ *Dll.*
 ┗━━━━━━
-
-*SISTEM: TES BOT-MASUKIN BOT-TF-DONE*
-
-*MINAT? CHAT :
-wa.me/6285609233482
 `
 }
